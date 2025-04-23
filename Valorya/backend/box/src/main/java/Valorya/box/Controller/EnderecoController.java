611c23754aca404207d319ae246396931a5f0bd3
Adrian3dds <adriansilva071@gmail.com>
2025-04-23 07:55:01 -0300

    Delete Valorya/backend/box directory

--- Valorya/backend/box/src/main/java/Valorya/box/Controller/EnderecoController.java
+++
@@ -1,9 +0,0 @@
-package Valorya.box.Controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/Endereco")
-public class EnderecoController {
-}
--- Valorya/backend/box/src/main/java/Valorya/box/Repository/EnderecoRepository.java
+++
@@ -1,8 +0,0 @@
-package Valorya.box.Repository;
-
-import Valorya.box.Entity.Endereco;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnderecoRepository extends JpaRepository <Endereco,Long> {
-
-}
